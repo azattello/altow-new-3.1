b6338c2270ce28b7b90e73b45796aef034212735
Abdykali Azat <azat.abdykalli@gmail.com>
2024-06-06 07:46:59 +0500

    altow commit cl

---
+++ src/config.js
@@ -0,0 +1,11 @@
+// config.js
+const config = {
+    // apiUrl: 'http://localhost:3001',
+    apiUrl: 'https://altow-cargo.kz:3001',
+    
+    nameCargo: 'Altow-Cargo'
+  };
+  
+  
+export default config;
+  
